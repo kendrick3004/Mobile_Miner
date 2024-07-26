64582410495483ab2c3ffae552eae0868fd36b6b
kendrick3004 <101604411+kendrick3004@users.noreply.github.com>
2024-07-26 14:14:48 -0300

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+echo "Replace dero1qyqra0mplgd70vmpg03d05uxukm8rvynwsgtnshj25dw0k3gfl326qqy4x2su, dero-node-gustavogerman.mysrv.cloud:10300 to run the miner"
 while :; do
-    ./astrominer -w YOUR_ADDRESS -r YOUR_NODE:YOUR_PORT -p rpc;
+    ./astrominer -w dero1qyqra0mplgd70vmpg03d05uxukm8rvynwsgtnshj25dw0k3gfl326qqy4x2su -r dero-node-gustavogerman.mysrv.cloud:10300 -m 7 -p rpc;
     sleep 5;
 done
